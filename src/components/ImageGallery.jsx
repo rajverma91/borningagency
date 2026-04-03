29e13f1ef5e74899f005d58f3a4af4c10c5725cb
theAD-dev <dev@thead.com.au>
2026-04-03 20:41:27 +0530

    newcommit123447

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,16 +2,15 @@
 
 
 const images = [
-   "https://framerusercontent.com/images/kFPS1Qb6vl1AWJTvqXVAT5vhxWY.png",
-  "https://framerusercontent.com/images/BReNkikWaCJZF2Bj04jLXGn26w.png",
-  "https://framerusercontent.com/images/MI3z40IAAs8YpJ39EtPI479jW4A.png",
-  "https://framerusercontent.com/images/DLrAVWztrtoKQ8NSNMg1Uaiurw.png",
-  "https://framerusercontent.com/images/82Sov4wFOHIoNVFxGYqBQSAX5s.png",
+  "https://res.cloudinary.com/deydhfxgg/image/upload/v1775227385/w12_d4m3b1.png",
+  "https://res.cloudinary.com/deydhfxgg/image/upload/v1775227385/w16_rywiwf.png",
+  "https://res.cloudinary.com/deydhfxgg/image/upload/v1775227385/w13_ghnd19.png",
+  "https://res.cloudinary.com/deydhfxgg/image/upload/v1775227383/w15_qkkvkv.png",
 ];
 
 export default function ImageGallery() {
   return (
-    <div className="w-full overflow-x-auto scrollbar-hide">
+    <div className="w-full overflow-hidden"> ">
       <div className="flex gap-6 px-4 py-6 min-w-max">
         {images.map((src, index) => (
           <div
